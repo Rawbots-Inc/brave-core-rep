56a25b47b7b29c95def632d4960fc8e6a2a3f91f
dang <dangnguyenhoathuan@gmail.com>
2025-07-04 02:02:47 -0700

    update: Rep Social to RepSky

--- android/java/org/chromium/chrome/browser/notifications/retention/RetentionNotificationUtil.java
+++ android/java/org/chromium/chrome/browser/notifications/retention/RetentionNotificationUtil.java
@@ -29,7 +29,7 @@ import java.util.Map;
 
 public class RetentionNotificationUtil {
     public static String NOTIFICATION_TYPE = "notification_type";
-    private static final String BRAVE_BROWSER = "Rep Social";
+    private static final String BRAVE_BROWSER = "RepSky";
 
     public static final String HOUR_3 = "hour_3";
     public static final String HOUR_24 = "hour_24";

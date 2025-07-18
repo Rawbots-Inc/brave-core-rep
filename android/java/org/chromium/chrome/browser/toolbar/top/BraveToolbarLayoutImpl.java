b600671a19fabe0033dac7b1f077706617f3f4a8
dang <dangnguyenhoathuan@gmail.com>
2025-07-18 03:10:46 -0700

    update: url

--- android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
+++ android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
@@ -1315,15 +1315,15 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
              ChromeSharedPreferences.getInstance()
       .writeBoolean(BraveRewardsPanel.PREF_WAS_TOOLBAR_BAT_LOGO_BUTTON_PRESSED, true);
       String targetUrl = (url != null && !url.isEmpty()) ?
-      "https://prod-extension.rsky.ai?currentTabUrl=" + transformToDesktopURL(url) :
-      "https://prod-extension.rsky.ai?currentTabUrl=newtab";
+      "https://dev-extension.rsky.ai?currentTabUrl=" + transformToDesktopURL(url) :
+      "https://dev-extension.rsky.ai?currentTabUrl=newtab";
     showOnBoarding(targetUrl);
    
   } else {
     Log.d(TAG, "Button Brave Rewards ko phai dau dien");
  String targetUrl = (url != null && !url.isEmpty()) ?
-      "https://prod-extension.rsky.ai?currentTabUrl=" + transformToDesktopURL(url) :
-      "https://prod-extension.rsky.ai?currentTabUrl=newtab";
+      "https://dev-extension.rsky.ai?currentTabUrl=" + transformToDesktopURL(url) :
+      "https://dev-extension.rsky.ai?currentTabUrl=newtab";
     CustomTabActivity.showInfoPage(getContext(), targetUrl);
     
 

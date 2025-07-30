96d09a96b47d452eb1b604fb46486cb9431e4011
dang <dangnguyenhoathuan@gmail.com>
2025-07-29 18:05:32 -0700

    update: comment code

--- android/java/org/chromium/chrome/browser/tabbed_mode/BraveTabbedAppMenuPropertiesDelegate.java
+++ android/java/org/chromium/chrome/browser/tabbed_mode/BraveTabbedAppMenuPropertiesDelegate.java
@@ -306,19 +306,19 @@ public class BraveTabbedAppMenuPropertiesDelegate extends TabbedAppMenuPropertie
                             R.id.brave_wallet_id,
                             R.id.all_bookmarks_menu_id));
         }
-        if (BraveLeoPrefUtils.isLeoEnabled()) {
-            Tab tab = mActivityTabProvider.get();
-            if (tab != null && !tab.isIncognito()) {
-                addMenuItemAfter(
-                        modelList,
-                        buildBraveLeoItem(),
-                        Arrays.asList(
-                                R.id.add_to_playlist_id,
-                                R.id.brave_playlist_id,
-                                R.id.brave_wallet_id,
-                                R.id.all_bookmarks_menu_id));
-            }
-        }
+        // if (BraveLeoPrefUtils.isLeoEnabled()) {
+        //     Tab tab = mActivityTabProvider.get();
+        //     if (tab != null && !tab.isIncognito()) {
+        //         addMenuItemAfter(
+        //                 modelList,
+        //                 buildBraveLeoItem(),
+        //                 Arrays.asList(
+        //                         R.id.add_to_playlist_id,
+        //                         R.id.brave_playlist_id,
+        //                         R.id.brave_wallet_id,
+        //                         R.id.all_bookmarks_menu_id));
+        //     }
+        // }
         if (ChromeFeatureList.isEnabled(BraveFeatureList.BRAVE_SPEEDREADER)
                 && UserPrefs.get(assumeNonNull(mTabModelSelector.getCurrentModel().getProfile()))
                         .getBoolean(BravePref.SPEEDREADER_PREF_ENABLED)) {
@@ -464,14 +464,14 @@ public class BraveTabbedAppMenuPropertiesDelegate extends TabbedAppMenuPropertie
                         shouldShowIconBeforeItem() ? R.drawable.ic_readermode : 0));
     }
 
-    private MVCListAdapter.ListItem buildBraveLeoItem() {
-        return new MVCListAdapter.ListItem(
-                AppMenuHandler.AppMenuItemType.STANDARD,
-                buildModelForStandardMenuItem(
-                        R.id.brave_leo_id,
-                        R.string.menu_brave_leo,
-                        shouldShowIconBeforeItem() ? R.drawable.ic_brave_ai : 0));
-    }
+    // private MVCListAdapter.ListItem buildBraveLeoItem() {
+    //     return new MVCListAdapter.ListItem(
+    //             AppMenuHandler.AppMenuItemType.STANDARD,
+    //             buildModelForStandardMenuItem(
+    //                     R.id.brave_leo_id,
+    //                     R.string.menu_brave_leo,
+    //                     shouldShowIconBeforeItem() ? R.drawable.ic_brave_ai : 0));
+    // }
 
     private MVCListAdapter.ListItem buildBraveVpnItem() {
         return new MVCListAdapter.ListItem(

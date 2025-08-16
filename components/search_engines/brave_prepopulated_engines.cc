7e4b49dd3e13a2dc00e9eae27d0aa12d13e92f89
dang <dangnguyenhoathuan@gmail.com>
2025-08-16 06:53:34 -0700

    update: url

--- components/search_engines/brave_prepopulated_engines.cc
+++ components/search_engines/brave_prepopulated_engines.cc
@@ -209,16 +209,16 @@ const PrepopulatedEngine brave_yandex =
 const PrepopulatedEngine brave_search = MakeBravePrepopulatedEngine(
     u"Brave",
     u":br",
-    "https://cdn.search.brave.com/serp/favicon.ico",
+    "https://cdn.search.google.com/serp/favicon.ico",
     "IDR_SEARCH_ENGINE_BRAVE",
-    "https://search.brave.com/search?q={searchTerms}&source="
+    "https://search.google.com/search?q={searchTerms}&source="
 #if BUILDFLAG(IS_ANDROID)
     "android",
 #else
     "desktop",
 #endif
     "UTF-8",
-    "https://search.brave.com/api/"
+    "https://search.google.com/api/"
     "suggest?q={searchTerms}&rich=true&source="
 #if BUILDFLAG(IS_ANDROID)
     "android",

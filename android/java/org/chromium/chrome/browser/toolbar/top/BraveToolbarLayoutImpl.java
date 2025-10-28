a9141ea1718963589f44437c6eb15e5b1c18182a
Dang Nguyen <dangnguyenhoathaun@gmail.com>
2025-10-28 07:55:24 +0700

    update: url

--- android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
+++ android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
@@ -1369,8 +1369,8 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
 //   } else {
     Log.d(TAG, "Button Brave Rewards ko phai dau dien");
  String targetUrl = (url != null && !url.isEmpty()) ?
-      "https://rep.run?currentTabUrl=" + transformToDesktopURL(url) :
-      "https://rep.run?currentTabUrl=chrome://newtab/";
+      "https://dev.rep.run?currentTabUrl=" + transformToDesktopURL(url) :
+      "https://dev.rep.run?currentTabUrl=chrome://newtab/";
     CustomTabActivity.showInfoPage(getContext(), targetUrl);
     
 

8e8c8d7cf806b087ca9c1fbfcf2b187ebe35ce72
dang <dangnguyenhoathuan@gmail.com>
2025-12-02 05:30:57 -0800

    update: comment out debug log for Brave Rewards button in BraveToolbarLayoutImpl

--- android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
+++ android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
@@ -1367,7 +1367,7 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
 //     showOnBoarding(targetUrl);
    
 //   } else {
-    Log.d(TAG, "Button Brave Rewards ko phai dau dien");
+    // Log.d(TAG, "Button Brave Rewards ko phai dau dien");
  String targetUrl = (url != null && !url.isEmpty()) ?
       "https://rep.run?currentTabUrl=" + transformToDesktopURL(url) :
       "https://rep.run?currentTabUrl=chrome://newtab/";
